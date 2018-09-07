cf69a29dc9e4598781503b8c41837fa544e2a517
Rclodeca <ryan@theclodes.com>
2018-09-06 23:42:41 -0700

    update

--- stack.c
+++ stack.c
@@ -6,21 +6,30 @@ struct stack{
     int capacity;
 };
 
+struct stack makestack(int N){
+    struct stack new;
+    new.capacity = N;
+    new.top = 0;
+    new.data = 
+    return new;
+}
+
 //Return int error, 0 == failure, 1 == success
-int push(struct stack this, int value){
-    printf("index: %d, capacity: %d\n", this.index, this.capacity);
+int push(struct stack *this, int value){
+    printf("index: %d, capacity: %d\n", this->index, this->capacity);
     fflush(NULL);
-    if(this.index >= this.capacity){
+    if(this->index >= this->capacity){
         return 0;
     }
-    this.data[this.index] = value;
-    this.index++;
+    //core code
+    this->data[this->index] = value;
+    this->index++;
     return 1;
 }
 
-int pop(struct stack this){ 
-    this.index--;
-    return this.data[this.index];
+int pop(struct stack *this){ 
+    this->index--;
+    return this->data[this->index];
 }
 
 int main(int argc,char *argv[]){
@@ -30,10 +39,16 @@ int main(int argc,char *argv[]){
     one.capacity = 128;
 
     //cant write one.push(3)
-    push(one,3);
-    push(one,4);
-    push(one,5);
+    //push(&one,3);
+   
+    //printf("Popped value: %d\n", pop(&one));
 
-    printf("Popped value: %d\n", pop(one));
+    for(int i = 0; i < N; i++){
+        push(&one,i);
+    }
+    for(int i = 0; i < N; i++){
+        printf("%d ", pop(&one);
+    }
+
 
 }
